e1af669d1f664ea2d8f376b35908dbb14fded0d5
Robino-CK <robin.kollmann@yahoo.de>
2024-06-26 19:45:30 +0200

    added intercept

--- data_processing.py
+++ data_processing.py
@@ -1,5 +1,8 @@
 import pandas as pd
 
+from sklearn.model_selection import train_test_split
+
+
 def load_nasa():
     df = pd.read_csv('res/data/nasa.csv')
     # Drop columns with irrelevant information, repetitions (such as KM and Miles) and static values (such as 'Orbiting Body' and 'Equinox')
@@ -19,3 +22,37 @@ def load_nasa():
 
 def load_gas():
     df = pd.read_csv('res/data/Gas_Turbine/gt_2011.csv')
+    
+    
+
+def prepare_data(X, y):
+    x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+        
+    # Get mean and standard deviation from train data
+    X_means = x_train.mean()
+
+    X_std = x_train.std()
+
+    # Normalize train and test data (Centering, unit variance)
+    x_train = x_train.apply(lambda x: (x-X_means)/X_std, axis = 1)
+    x_test = x_test.apply(lambda x: (x-X_means)/X_std, axis = 1)
+    return x_train, x_test, y_train, y_test
+
+def normalize_data(values):
+    ebm_values = []
+    min_ebm = min(values)
+    max_ebm = max(values)
+    for v in values:
+        ebm_values.append((v-min_ebm)/(max_ebm-min_ebm))
+    return ebm_values
+
+
+def get_data():
+    X , y = load_nasa()
+    return prepare_data(X, y)
+
+
+def data_selected_features():
+    X,y = load_nasa()
+    X = X[["Minimum Orbit Intersection", "Absolute Magnitude", "Est Dia in KM(min)", "Eccentricity", "Mean Motion", "Inclination", "Perihelion Distance"]]
+    return prepare_data(X, y)
---
+++ models.py
@@ -0,0 +1,8 @@
+
+from sklearn.neural_network import MLPRegressor, MLPClassifier
+
+
+def nn_model():
+    parameters = {'activation': 'relu', 'alpha': 0.1, 'hidden_layer_sizes': (20,), 'learning_rate_init': 0.1, 'solver': 'sgd'}
+    mlp = MLPClassifier(** parameters)
+    return mlp
