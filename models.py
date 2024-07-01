7d35ae03099c2ab28459842fb8045faeaa9aac1f
Robino-CK <robin.kollmann@yahoo.de>
2024-07-01 16:55:35 +0200

    final commit

--- models.py
+++ models.py
@@ -3,6 +3,6 @@ from sklearn.neural_network import MLPRegressor, MLPClassifier
 
 
 def nn_model():
-    parameters = {'activation': 'relu', 'alpha': 0.1, 'hidden_layer_sizes': (20,), 'learning_rate_init': 0.1, 'solver': 'sgd'}
+    parameters = {'activation': 'relu', 'alpha': 0.1, 'hidden_layer_sizes': (20,2), 'learning_rate_init': 0.1, 'solver': 'sgd'}
     mlp = MLPClassifier(** parameters)
     return mlp
--- plot_helper.py
+++ plot_helper.py
@@ -8,13 +8,13 @@ def plot_distribution(distributions, header):
     plt.title(header)
     plt.savefig(f"res/plots/hist_{header}.png")
     plt.show()
-    
+
 def plot_bar(shap_values, ebm_values, pfi_values, columns, header="test"):
     #fig, ax = plt.subplots()
     df = pd.DataFrame({"feature" : columns, "EBM": ebm_values,"NN (SHARP)": shap_values , "NN (PFI)" : pfi_values} )
-    df.sort_values(by=["NN (SHARP)"], inplace = True)
+    df.sort_values(by=["feature"], ascending = False, inplace = True)
     
-    df.plot.barh(x = 'feature', y = ['EBM', 'NN (SHARP)', "NN (PFI)"], color={"EBM": "#FFCD00", "NN (SHARP)": "#AA1555", "NN (PFI)": "#5287C6"})
+    df.plot.barh(x = 'feature', y = ['Normalgaus'], color={"Normalgaus": "#FFCD00", "Normalgaus": "#AA1555", "": "#5287C6"})
     plt.xlabel("Feature Importance")
     plt.yticks(rotation=45)
     plt.ylabel("Features")
@@ -23,3 +23,33 @@ def plot_bar(shap_values, ebm_values, pfi_values, columns, header="test"):
     plt.savefig(f"res/plots/barh_{header}.png")
     plt.show()
     
+    
+def plot_bar_per_scale(metric_values,columns,experiments=["Baseline", "Normaldistribution", "Weibulldistribution"], experiments_names = None,  header="test"):
+    #fig, ax = plt.subplots()
+    if not experiments_names:
+        experiments_names = experiments
+    d = {}
+    color_list = {"#FFCD00" ,"#AA1555", "#5287C6", "#000000"}
+    d_colors = {}
+    
+    for exp_n,exp in zip(experiments_names,experiments):
+        
+        
+        d[exp_n] = metric_values[f"{exp}"]
+        d_colors[exp_n] = color_list.pop()
+        if color_list == set():
+            color_list.add("#000000")
+        
+    
+    d["feature"] = columns
+    df = pd.DataFrame(d )
+    df.sort_values(by=["feature"], ascending = False, inplace = True)
+    
+    df.plot.barh(x = 'feature', y = experiments_names , color=d_colors, title=header)
+    plt.xlabel("Feature Importance")
+    plt.yticks(rotation=45)
+    plt.ylabel("Features")
+    plt.legend(loc='right')
+    #ax.legend("EBM": "Feature Importance EBM","NN": "Feature Importance NN"}
+    plt.savefig(f"res/plots/barh_{header}.png")
+    plt.show()
